d503f30b5b98070c0e23464a7a51750d1030a48a
Hailyy <82746183+HailyyDev@users.noreply.github.com>
2021-04-25 13:38:21 +0100

    Update ban.js

--- src/commands/Test/ban.js
+++ src/commands/Test/ban.js
@@ -4,18 +4,20 @@ module.exports = {
    aliases: ["b"],
    timeout: 3000,
    run: async(client, message, args) => {
-     if(!message.guild.me.hasPermission("BAN_MEMBERS")) return message.reply("I do not have the **Ban Members** permission. Please make sure you enable it so I can run this command.");
-     if(!message.member.hasPermission("BAN_MEMBERS")) return message.reply("You do not have the **Ban Members** permission.")
+     if (!message.guild.me.hasPermission("BAN_MEMBERS")) return message.reply("I do not have the **Ban Members** permission. Please make sure you enable it so I can run this command.");
+     if (!message.member.hasPermission("BAN_MEMBERS")) return message.reply("You do not have the **Ban Members** permission.")
      
      let user = message.mentions.members.first();
-     if(!user) return message.channel.send("That is not a valid user!");
+     if (!user) return message.channel.send("That is not a valid user!");
      
      let reason = args.slice(1).join(" ");
-     if(!reason) return message.channel.send("I do not see a reason.");
+     if (!reason) return message.channel.send("I do not see a reason.");
      
+     let banned = new Discord.MessageEmbed()
+     .setDescription(`Successfully banned ${user} for the reason of: \`${}\``)
+      
      try {
-     
-     
+      
      } catch (e) {
      
      }
